0aaa7c9dcff0bc041dc3b421bd3ae8bdb2e99b9f
rajibdas-it <rajibrad@gmail.com>
2022-09-26 17:49:40 +0600

    cart section complete

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -5,10 +5,11 @@ const Cart = (props) => {
   const { cart } = props;
   let total = 0;
   let shipping = 0;
+  let quantity = 0;
 
   for (const product of cart) {
-    console.log(product);
-    total = total + product.price;
+    quantity = quantity + product.quantity;
+    total = total + product.price * product.quantity;
     shipping = shipping + product.shipping;
   }
   //   const tax = (total + shipping) * 0.1;
@@ -18,7 +19,7 @@ const Cart = (props) => {
   return (
     <div className="cart">
       <h4>Order Summary</h4>
-      <p>Selected Items: {cart.length}</p>
+      <p>Selected Items: {quantity}</p>
       <p>Total Price: ${total.toFixed(2)}</p>
       <p>Total Shipping: ${shipping.toFixed(2)}</p>
       <p>Tax: ${tax.toFixed(2)}</p>
